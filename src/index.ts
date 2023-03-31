49858771cc3ca376c1f9a33296d3a3b831ce11f5
EgoFox <egotdp4@gmail.com>
2023-03-31 22:23:15 +0200

    Add some CSS and HTML + add localStorage for tasks

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,9 @@ const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
 form?.addEventListener("submit", e => {
     e.preventDefault()  
 
@@ -27,19 +30,38 @@ form?.addEventListener("submit", e => {
         completed: false,
         createdAt: new Date()
     }
+
+    tasks.push(newTask)
+
     addListItem(newTask)
+    input.value = ""
 })
 
 function addListItem(task: Task){
     const item = document.createElement("li")
     const label = document.createElement("label")
     const checkbox = document.createElement("input")
+    checkbox.addEventListener("change", () => {
+        task.completed = checkbox.checked 
+        saveTasks()
+    })
     checkbox.type = "checkbox"
+    checkbox.checked = task.completed
     label.append(checkbox, task.title)
     item.append(label)
     list?.append(item)
+    saveTasks()
 }
-//please, make a button here
+
+function saveTasks(){
+    localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem("TASKS")
+    if(taskJSON == null) return []
+    return JSON.parse(taskJSON)
+}
 
 console.log("Hiii!")
 console.log("Hiii!")
